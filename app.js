44df17fe66a939bccc5d7c0090e0eb13896b0a4e
Bhavin-Kareliya <bhavin.kareliya17977@marwadieducation.edu.in>
2022-02-03 17:06:47 +0530

    Update design and user experince by adding new icons and animations

--- app.js
+++ app.js
@@ -1,28 +1,39 @@
-angular.module("todoApp", [])
-	.controller('TodoListController', ($scope) => {
-		$scope.title = "Todo App";
-
-		$scope.todoList = [
-			{ task: "Have 3 Lectures, 1 Lab", isCompleted: false },
-			{ task: "Meeting at 5 O'clock", isCompleted: false },
-			{ task: "Explore New Things", isCompleted: false },
-			{ task: "Attend function on 28 Jan", isCompleted: false },
-		];
-
-		$scope.deleteTask = (taskId) => {
-			$scope.todoList.splice(taskId, 1);
+angular.module("todoApp", []).controller("TodoListController", ($scope) => {
+	$scope.title = "Things to be done";
+	$scope.todoList = [
+		{ task: "Have 3 Lectures, 1 Lab", isCompleted: false },
+		{ task: "Meeting at 5 O'clock", isCompleted: false },
+		{ task: "Explore New Things", isCompleted: false },
+		{ task: "Attend function on 28 Jan", isCompleted: false },
+	];
+	$scope.remaining = $scope.todoList.filter((task) => task.isCompleted == false).length;
+	$scope.deleteTask = (taskId) => {
+		$scope.todoList.splice(taskId, 1);
+	};
+	$scope.deleteAll = () => {
+		$scope.todoList = [];
+	};
+	$scope.deleteCompleted = () => {
+		$scope.todoList = $scope.todoList.filter((task) => task.isCompleted == false);
+	};
+	$scope.remaining = function () {
+		var count = 0;
+		angular.forEach($scope.todoList, (todo) => {
+			count += todo.isCompleted ? 0 : 1;
+		});
+		if (count > 0) {
+			if (count == 1) {
+				return count + " task remaining out of " + $scope.todoList.length;
+			} else {
+				return count + " tasks remaining out of " + $scope.todoList.length;
+			}
+		} else {
+			return "All tasks are done.";
 		}
-
-		$scope.deleteAll = () => {
-			$scope.todoList = [];
-		}
-
-		$scope.deleteCompleted = () => {
-			$scope.todoList = $scope.todoList.filter(task => task.isCompleted == false);
-		}
-
-		$scope.addTask = (e) => {
-			console.log("comming");
-
-		}
-	})
+	};
+	$scope.addTask = () => {
+		var obj = { task: $scope.todoList.task, isCompleted: false };
+		$scope.todoList.unshift(obj);
+		$scope.todoList.task = "";
+	};
+});
